e11d45f1cac607b0ab78d2375c8e68eccfaf0654
N1Anoobis <mrbond84@hotmail.com>
2020-03-03 20:10:54 +0100

    Add whole structure of aplication, display game result

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 
-
+//player choise
 let playerMove = 'nieznany ruch';
 
 if (playerInput == '1') {
     playerMove = 'kamień';
-}else if (playerInput == 2) {
+} else if (playerInput == 2) {
     playerMove = 'papier'
 } else if (playerInput == 3) {
     playerMove = 'nozyce'
@@ -19,7 +19,7 @@ printMessage('Twój ruch to: ' + playerMove);
 
 
 
-
+//computer choise
 let computerMove = 'nieznany ruch';
 
 if (randomNumber == 1) {
@@ -36,20 +36,27 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 printMessage('Mój ruch to: ' + computerMove);
 
 
+// logic
+if (playerMove == 'nieznany ruch') {
+    printMessage('wpisz liczbe 1, 2 lub 3');
+
+} else {
+
+    if (computerMove == playerMove) {
+        printMessage('Remis!');
+    } else if (computerMove == 'kamień' && playerMove == 'papier') {
+        printMessage('Ty wygrywasz!');
+    } else if (computerMove == 'papier' && playerMove == 'nozyce') {
+        printMessage('Ty wygrywasz!');
+    } else if (computerMove == 'nozyce' && playerMove == 'kamień') {
+        printMessage('Ty wygrywasz!');
+    } else {
+        printMessage('Przegrywasz');
+    }
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+//display
 
 function printMessage(msg) {
     let div = document.createElement('div');
