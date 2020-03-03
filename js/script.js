47879f72c02dd006c22e3261b80a7eded91697e8
N1Anoobis <mrbond84@hotmail.com>
2020-03-03 19:12:05 +0100

    Add computer draw and player choice else if-s

--- js/script.js
+++ js/script.js
@@ -1,15 +1,62 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+}else if (playerInput == 2) {
+    playerMove = 'papier'
+} else if (playerInput == 3) {
+    playerMove = 'nozyce'
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+printMessage('Twój ruch to: ' + playerMove);
+
+
+
+
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+    computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier'
+} else if (randomNumber == 3) {
+    computerMove = 'nozyce'
+}
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+
+printMessage('Mój ruch to: ' + computerMove);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function printMessage(msg) {
+    let div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages() {
+    document.getElementById('messages').innerHTML = '';
 }
