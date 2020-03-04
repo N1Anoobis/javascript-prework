43487b845571aa45a0701a37ba0a60783f58e338
N1Anoobis <mrbond84@hotmail.com>
2020-03-04 12:09:54 +0100

    Add buttons and event listeners

--- js/script.js
+++ js/script.js
@@ -1,61 +1,67 @@
-function getMoveName(argMoveId) {
-    console.log(argMoveId);
-    if (argMoveId == 1) {
-        return 'kamień';
-    } else if (argMoveId == 2) {
-        return 'papier';
-    } else if (argMoveId == 3) {
-        return 'nozyce';
+function playGame(playerInput) {
+    function getMoveName(argMoveId) {
         console.log(argMoveId);
+        if (argMoveId == 1) {
+            return 'kamień';
+        } else if (argMoveId == 2) {
+            return 'papier';
+        } else if (argMoveId == 3) {
+            return 'nozyce';
+        }
+    }
+    //
+    function displayResult(argComputerMove, argPlayerMove) {
+        printMessage(`Zagrałem ${argComputerMove}, a Ty ${argPlayerMove}`);
+
+        if (argComputerMove == argPlayerMove) {
+            printMessage('Remis!');
+        } else if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+            printMessage('Ty wygrywasz!');
+        } else if (argComputerMove == 'papier' && argPlayerMove == 'nozyce') {
+            printMessage('Ty wygrywasz!');
+        } else if (argComputerMove == 'nozyce' && argPlayerMove == 'kamień') {
+            printMessage('Ty wygrywasz!');
+        } else {
+            printMessage('Przegrywasz');
+        }
+
+    }
+    //beagining of program
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    clearMessages()
+
+
+    //player choise passed to getMoveName
+    let playerMove = getMoveName(playerInput);
+
+
+    //computer choise passed to getMoveName
+    let computerMove = getMoveName(randomNumber);
+
+
+    // player and computer args passed to displayResult
+    displayResult(computerMove, playerMove);
+
+
+    //display in browser
+    function printMessage(msg) {
+        let div = document.createElement('div');
+        div.innerHTML = msg;
+        document.getElementById('messages').appendChild(div);
     }
 
-    printMessage(`Nie znam ruchu o id ${argMoveId} .`);
-    return 'nieznany ruch';
-}
-//
-function displayResult(argComputerMove, argPlayerMove) {
-    console.log('moves:', argComputerMove, argPlayerMove);
-    printMessage(`Zagrałem ${argComputerMove}, a Ty ${argPlayerMove}`);
-
-
-    if (argComputerMove == argPlayerMove) {
-        printMessage('Remis!');
-    } else if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
-        printMessage('Ty wygrywasz!');
-    } else if (argComputerMove == 'papier' && argPlayerMove == 'nozyce') {
-        printMessage('Ty wygrywasz!');
-    } else if (argComputerMove == 'nozyce' && argPlayerMove == 'kamień') {
-        printMessage('Ty wygrywasz!');
-    } else {
-        printMessage('Przegrywasz');
+    function clearMessages() {
+        document.getElementById('messages').innerHTML = '';
     }
-
-}
-//beagining of program
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-
-//player choise passed to getMoveName
-let playerMove = getMoveName(playerInput);
-
-
-//computer choise passed to getMoveName
-let computerMove = getMoveName(randomNumber);
-
-
-// player and computer args passed to displayResult
-displayResult(computerMove, playerMove);
-
-
-//display in browser
-function printMessage(msg) {
-    let div = document.createElement('div');
-    div.innerHTML = msg;
-    document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages() {
-    document.getElementById('messages').innerHTML = '';
-}
+// enent listiners
+document.getElementById('play-rock').addEventListener('click', function () {
+    playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function () {
+    playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function () {
+    playGame(3);
+});
