1a1e3d38888b16c027e88f41af5d37190e2a9a6f
N1Anoobis <mrbond84@hotmail.com>
2020-03-04 22:15:18 +0100

    Add loaderDelay function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function playGame(playerInput) {
     function getMoveName(argMoveId) {
-        // console.log(argMoveId);
+
         if (argMoveId == 1) {
             return 'kamień';
         } else if (argMoveId == 2) {
@@ -16,13 +16,16 @@ function playGame(playerInput) {
         if (argComputerMove == argPlayerMove) {
             printMessage('Remis!');
             counter('draw');
-            
+
         } else if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
-            printMessage('Ty wygrywasz!'); counter('win');
+            printMessage('Ty wygrywasz!');
+            counter('win');
         } else if (argComputerMove == 'papier' && argPlayerMove == 'nozyce') {
-            printMessage('Ty wygrywasz!'); counter('win');
+            printMessage('Ty wygrywasz!');
+            counter('win');
         } else if (argComputerMove == 'nozyce' && argPlayerMove == 'kamień') {
-            printMessage('Ty wygrywasz!'); counter('win');
+            printMessage('Ty wygrywasz!');
+            counter('win');
         } else {
             printMessage('Przegrywasz');
             counter('lose');
@@ -30,27 +33,27 @@ function playGame(playerInput) {
 
     }
 
+
+
     function counter(score) {
-       games += 1;
-       console.log(games)
-        if(score === 'win') {
-          let setWins = document.getElementById("wins");
-          setWins.innerHTML = wins += 1;
+        games += 1;
+        console.log(games)
+        if (score === 'win') {
+            let setWins = document.getElementById("wins");
+            setWins.innerHTML = wins += 1;
+        } else if (score === 'lose') {
+
+            let setLosses = document.getElementById("losses");
+            setLosses.innerHTML = losses += 1;
+        } else if (score === 'draw') {
+            let setDraws = document.getElementById("draws");
+            setDraws.innerHTML = draws += 1;
         }
-        else if(score === 'lose') {
-            
-          let setLosses = document.getElementById("losses");
-          setLosses.innerHTML = losses += 1;
-        }
-        else if(score === 'draw'){
-          let setDraws = document.getElementById("draws");
-          setDraws.innerHTML = draws += 1;
-        }
-      }
+    }
     //beagining of program
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     clearMessages()
-    
+
     // console.log(games)
     //player choise passed to getMoveName
     let playerMove = getMoveName(playerInput);
@@ -64,31 +67,34 @@ function playGame(playerInput) {
     displayResult(computerMove, playerMove);
 
 
-    //display in browser
-    function printMessage(msg) {
-        let div = document.createElement('div');
-        div.innerHTML = msg;
-        document.getElementById('messages').appendChild(div);
-
-    }
-
-    function clearMessages() {
-        document.getElementById('messages').innerHTML = '';
-     
-    }
-   
 }
 let games = 0;
-    let wins = 0;
-    let draws = 0;
-    let losses = 0;
+let wins = 0;
+let draws = 0;
+let losses = 0;
 // enent listiners
 document.getElementById('play-rock').addEventListener('click', function () {
+
+    loaderDelay()
     playGame(1);
 });
 document.getElementById('play-paper').addEventListener('click', function () {
+    loaderDelay()
     playGame(2);
 });
 document.getElementById('play-scissors').addEventListener('click', function () {
+    loaderDelay()
     playGame(3);
 });
+
+function loaderDelay() {
+    document.getElementById("scores").style.display = "none";
+    document.getElementById("messages").style.display = "none";
+    document.getElementById("loader").style.display = "block";
+    setTimeout(function () {
+        document.getElementById("scores").style.display = "flex";
+        document.getElementById("messages").style.display = "block";
+        document.getElementById("loader").style.display = "none"
+    }, 1000);
+
+};
